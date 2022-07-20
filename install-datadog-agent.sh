0f41b042966f3bfefcc7cabccb0ae2d3fa9db5f9
krust_Robert.Seo <krust@krustui-MacBookPro-10.local>
2022-07-20 14:37:03 +0800

    edit log_dir

--- install-datadog-agent.sh
+++ install-datadog-agent.sh
@@ -70,9 +70,6 @@ EOF
 if [ $NODE_TYPE == "cn" ]
 then
 
-conf=`find / -name 'kcnd.conf' -type f`
-LOG_DIR=`cat $conf | grep LOG_DIR | cut -d '=' -f 2 | tr -d ' '`
-
 mkdir -p /etc/datadog-agent/conf.d/go.d
 cat << EOF > /etc/datadog-agent/conf.d/go.d/conf.yaml
 #4. Log Config
